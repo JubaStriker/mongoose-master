cafb725bc80d6568f36c35c2554ac4ba2d93c366
JubaStriker <jubastriker@gmail.com>
2023-11-19 11:09:43 +0600

    Eslint running good

--- src/server.ts
+++ src/server.ts
@@ -11,9 +11,11 @@ async function main() {
             console.log(`Example app listening on port ${config.port}`)
         })
     }
-    catch (err: any) {
+    catch (err) {
         console.log(err);
     }
 }
+
+main();
 
 
