5ba4779751c77be3063ee39f122d536474157c59
JubaStriker <jubastriker@gmail.com>
2023-12-05 22:21:29 +0600

    Practice work added

--- src/app/modules/academicSemester/academicSemester.contoller.ts
+++ src/app/modules/academicSemester/academicSemester.contoller.ts
@@ -1,35 +1,63 @@
-import { NextFunction, Request, Response } from 'express';
-import sendResponse from '../../utils/sendResponse';
 import httpStatus from 'http-status';
+import catchAsync from '../../utils/catchAsync';
+import sendResponse from '../../utils/sendResponse';
 import { AcademicSemesterServices } from './academicSemester.services';
 
-const createAcademicSemester = async (
-  req: Request,
-  res: Response,
-  next: NextFunction,
-) => {
-  try {
-    const result = await AcademicSemesterServices.createAcademicSemesterIntoDb(
-      req.body,
-    );
-    sendResponse(res, {
-      success: true,
-      message: 'Academic semester created successfully',
-      data: result,
-      statusCode: httpStatus.OK,
-    });
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  } catch (e: any) {
-    next(e);
-    sendResponse(res, {
-      success: false,
-      message: e.message || 'Academic semester creation failed',
-      data: e,
-      statusCode: httpStatus.OK,
-    });
-  }
-};
+const createAcademicSemester = catchAsync(async (req, res) => {
+  const result = await AcademicSemesterServices.createAcademicSemesterIntoDB(
+    req.body,
+  );
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic semester is created successfully',
+    data: result,
+  });
+});
+
+const getAllAcademicSemesters = catchAsync(async (req, res) => {
+  const result = await AcademicSemesterServices.getAllAcademicSemestersFromDB();
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic semesters are retrieved successfully',
+    data: result,
+  });
+});
+
+const getSingleAcademicSemester = catchAsync(async (req, res) => {
+  const { semesterId } = req.params;
+  const result =
+    await AcademicSemesterServices.getSingleAcademicSemesterFromDB(semesterId);
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic semester is retrieved succesfully',
+    data: result,
+  });
+});
+
+const updateAcademicSemester = catchAsync(async (req, res) => {
+  const { semesterId } = req.params;
+  const result = await AcademicSemesterServices.updateAcademicSemesterIntoDB(
+    semesterId,
+    req.body,
+  );
+
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Academic semester is retrieved succesfully',
+    data: result,
+  });
+});
 
 export const AcademicSemesterControllers = {
   createAcademicSemester,
+  getAllAcademicSemesters,
+  getSingleAcademicSemester,
+  updateAcademicSemester,
 };
--- src/app/modules/academicSemester/academicSemester.services.ts
+++ src/app/modules/academicSemester/academicSemester.services.ts
@@ -1,25 +1,48 @@
+import { academicSemesterNameCodeMapper } from './academicSemester.constant';
 import { TAcademicSemester } from './academicSemester.interface';
 import { AcademicSemester } from './academicSemester.model';
-
-const createAcademicSemesterIntoDb = async (payload: TAcademicSemester) => {
-  // Semester name =-> semester code?
-  type TAcademicSemesterNameCodeMapper = {
-    [key: string]: string;
-  };
-
-  const academicSemesterNameCodeMapper: TAcademicSemesterNameCodeMapper = {
-    Autumn: '01',
-    Summer: '02',
-    Fall: '03',
-  };
-
+const createAcademicSemesterIntoDB = async (payload: TAcademicSemester) => {
+  // semester name --> semester code
+  // academicSemesterNameCodeMapper['Fall']
   if (academicSemesterNameCodeMapper[payload.name] !== payload.code) {
-    throw new Error('Invalid semester code');
+    throw new Error('Invalid Semester Code');
   }
+
   const result = await AcademicSemester.create(payload);
   return result;
 };
 
+const getAllAcademicSemestersFromDB = async () => {
+  const result = await AcademicSemester.find();
+  return result;
+};
+
+const getSingleAcademicSemesterFromDB = async (id: string) => {
+  const result = await AcademicSemester.findById(id);
+  return result;
+};
+
+const updateAcademicSemesterIntoDB = async (
+  id: string,
+  payload: Partial<TAcademicSemester>,
+) => {
+  if (
+    payload.name &&
+    payload.code &&
+    academicSemesterNameCodeMapper[payload.name] !== payload.code
+  ) {
+    throw new Error('Invalid Semester Code');
+  }
+
+  const result = await AcademicSemester.findOneAndUpdate({ _id: id }, payload, {
+    new: true,
+  });
+  return result;
+};
+
 export const AcademicSemesterServices = {
-  createAcademicSemesterIntoDb,
+  createAcademicSemesterIntoDB,
+  getAllAcademicSemestersFromDB,
+  getSingleAcademicSemesterFromDB,
+  updateAcademicSemesterIntoDB,
 };
